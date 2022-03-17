cefbcd17949a32dfe3f6b09dd7f4f5e15fd9f4b2
Andrada Sandu <andrada.sandu99@e-uvt.ro>
2022-03-17 21:43:33 +0200

    Ready for using

--- main.py
+++ main.py
@@ -1,33 +1,24 @@
-# import random
-#
-#
-#
-# start = int(input("Enter the start for the interval: "))
-# stop =  int(input("Enter the stop fot the interval: "))
-# if start < stop:
-#     for el in range(start, stop+1):
-#         print (el)
-# # try:
-# #     for el in reversed(range(start, stop+1)):
-# #         print(el)
-# # except ValueError:
-# #     print("oops")
-#
-# x = int(input("Enter the guess number: "))
-# y = random.randint(start, stop+1)
-# print(f'The random number is: {y}')
-# if x == y:
-#     print("It is corect. You are a genius!!")
-# else:
-#     print("It's incorect, try again!!")
+import random
 
 
 
-lst = [2, 4, 5, 7, 8]
+start = int(input("Enter the start for the interval: "))
+stop =  int(input("Enter the stop fot the interval: "))
+if start < stop:
+    for el in range(start, stop+1):
+        print (el)
+# try:
+#     for el in reversed(range(start, stop+1)):
+#         print(el)
+# except ValueError:
+#     print("oops")
 
-for i in range(len(lst)):
-    for item in range(len(lst)):
-        if i > item:
-                result = lst[i]
-print(result)
+x = int(input("Enter the guess number: "))
+y = random.randint(start, stop+1)
+print(f'The random number is: {y}')
+if x == y:
+    print("It is corect. You are a genius!!")
+else:
+    print("It's incorect, try again!!")
+
 
